b35d5fab759930daaeeb87ed63c273d362d94e1d
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-10-10 12:21:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const names =['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+   
+}
+
+function fuzzyMatch(drivers, letters){
+    const names = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+    return drivers.filter(driver => driver.startsWith(letters));
+
+}
+function matchName(drivers, name){
+    const names = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+    return drivers.filter(driver => driver.name === name);
+
+}
